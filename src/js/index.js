22d670289cc06b7bfd1fb601a78084366eb63259
YOSHIKI <vicugnapacos.g@gmail.com>
2020-07-05 16:20:20 +0900

    add setDirectory

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,43 @@
-const fs = require('fs');
-const { BrowserWindow, dialog } = require('electron').remote;
+const fs = require("fs");
+const { BrowserWindow, dialog } = require("electron").remote;
 
 //html内の要素取得とリスナーの設定
-document.querySelector("#openFile").addEventListener('click', () => {
-    openFile();
-})
+document.querySelector("#openFile").addEventListener("click", () => {
+  openDirectory();
+});
 
-const preview = document.getElementById('preview');
+const preview = document.getElementById("preview");
 
-//openFileボタンが押されたとき（ファイル名取得まで）
-function openFile() {
-    const win = BrowserWindow.getFocusedWindow();
-    dialog.showOpenDialog(
-        win,
-        {
-            properties: ['openDirectory'],
-        },
-        (fileNames) => {
-            if (fileNames) {
-                // alert(fileNames[0]);
-            }
+//Directoryボタンが押されたとき（ファイル名取得まで）
+function openDirectory() {
+  const win = BrowserWindow.getFocusedWindow();
+  dialog
+    .showOpenDialog(
+      win,
+      {
+        properties: ["openDirectory"],
+      },
+      (fileNames) => {
+        if (fileNames) {
+          // alert(fileNames[0]);
         }
-    ).then(function(result) {
-        var filePaths = result.filePaths;
-        if (filePaths) {
-            var target = document.getElementById('js-dirPath');
-            
-            if(filePaths[0]) {
-                target.innerHTML = filePaths[0];
-                target.dataset.path = filePaths[0];
-            } else {
-                target.innerHTML = 'ここにフォルダ名';
-                target.dataset.path = '';
-            }
-        }
+      }
+    )
+    .then(function (result) {
+      var filePaths = result.filePaths;
+      if (filePaths) {
+        setDirectory(filePaths[0]);
+      }
     });
 }
 
+function setDirectory(filePath) {
+  var target = document.getElementById("js-dirPath");
+  if (filePath) {
+    target.innerHTML = filePath;
+    target.dataset.path = filePath;
+  } else {
+    target.innerHTML = "ここにフォルダ名";
+    target.dataset.path = "";
+  }
+}
